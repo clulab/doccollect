1a27f4e3c89c18974e2c6a59c323939c217f0669
Keith Alcock <github@keithalcock.com>
2021-08-16 14:06:06 -0700

    Change urls

--- brat/doccollect/doccollect/urls.py
+++ brat/doccollect/doccollect/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
     path('admin/', admin.site.urls),
     path('docs/', include('docs.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
